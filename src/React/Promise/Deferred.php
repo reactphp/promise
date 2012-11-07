60a4379ffc322c7a00b10cee85604cf214ce9066
Jan Sorgalla <jan@sorgalla.com>
2012-11-07 10:13:25 +0100

    Some refactoring to prevent infinite loops

--- src/React/Promise/Deferred.php
+++ src/React/Promise/Deferred.php
@@ -12,35 +12,55 @@ class Deferred implements PromiseInterface, ResolverInterface
 
     public function then($fulfilledHandler = null, $errorHandler = null, $progressHandler = null)
     {
-        if (null !== $this->completed) {
-            return $this->completed->then($fulfilledHandler, $errorHandler, $progressHandler);
-        }
-
         $deferred = new static();
 
-        if ($progressHandler) {
-            $progHandler = function ($update) use ($deferred, $progressHandler) {
-                try {
-                    $deferred->progress(call_user_func($progressHandler, $update));
-                } catch (\Exception $e) {
-                    $deferred->progress($e);
-                }
-            };
+        if (null !== $this->completed) {
+            $completed = $this->completed;
+
+            $this->executeCallback(function () use ($completed, $fulfilledHandler, $errorHandler, $deferred) {
+                $completed
+                    ->then($fulfilledHandler, $errorHandler)
+                    ->then(
+                        function ($value) use ($deferred) {
+                            $deferred->resolve($value);
+                        },
+                        function ($reason) use ($deferred) {
+                            $deferred->reject($reason);
+                        },
+                        function ($update) use ($deferred) {
+                            $deferred->progress($update);
+                        }
+                    );
+            });
         } else {
-            $progHandler = array($deferred, 'progress');
+            if ($progressHandler) {
+                $progHandler = function ($update) use ($deferred, $progressHandler) {
+                    try {
+                        $deferred->progress(call_user_func($progressHandler, $update));
+                    } catch (\Exception $e) {
+                        $deferred->progress($e);
+                    }
+                };
+            } else {
+                $progHandler = array($deferred, 'progress');
+            }
+
+            $this->handlers[] = function ($promise) use ($fulfilledHandler, $errorHandler, $deferred, $progHandler) {
+                $promise
+                    ->then($fulfilledHandler, $errorHandler)
+                    ->then(
+                        function ($value) use ($deferred) {
+                            $deferred->resolve($value);
+                        },
+                        function ($reason) use ($deferred) {
+                            $deferred->reject($reason);
+                        },
+                        $progHandler
+                    );
+            };
+
+            $this->progressHandlers[] = $progHandler;
         }
-
-        $this->handlers[] = function ($promise) use ($fulfilledHandler, $errorHandler, $deferred, $progHandler) {
-            $promise
-                ->then($fulfilledHandler, $errorHandler)
-                ->then(
-                    array($deferred, 'resolve'),
-                    array($deferred, 'reject'),
-                    $progHandler
-                );
-        };
-
-        $this->progressHandlers[] = $progHandler;
 
         return $deferred->promise();
     }
@@ -60,7 +80,7 @@ class Deferred implements PromiseInterface, ResolverInterface
 
         $this->progressHandlers = $this->handlers = array();
 
-        return $this->completed;
+        return $this->promise();
     }
 
     public function reject($error = null)
@@ -98,7 +118,15 @@ class Deferred implements PromiseInterface, ResolverInterface
     protected function processQueue($queue, $value)
     {
         foreach ($queue as $handler) {
-            call_user_func($handler, $value);
+            $this->executeCallback($handler, $value);
         }
+    }
+
+    protected function executeCallback($callback/*, $args ... */)
+    {
+        $args = func_get_args();
+        array_shift($args);
+
+        call_user_func_array($callback, $args);
     }
 }
