1cd3a71271d79d2c990ae52cdb1c15284b854d7d
Jan Sorgalla <jsorgalla@gmail.com>
2015-04-08 13:14:51 +0200

    Return $this instead of new instances

--- src/RejectedPromise.php
+++ src/RejectedPromise.php
@@ -17,11 +17,11 @@ class RejectedPromise implements ExtendedPromiseInterface, CancellablePromiseInt
 
     public function then(callable $onFulfilled = null, callable $onRejected = null, callable $onProgress = null)
     {
-        try {
-            if (null === $onRejected) {
-                return new RejectedPromise($this->reason);
-            }
+        if (null === $onRejected) {
+            return $this;
+        }
 
+        try {
             return resolve($onRejected($this->reason));
         } catch (\Exception $exception) {
             return new RejectedPromise($exception);
@@ -48,7 +48,7 @@ class RejectedPromise implements ExtendedPromiseInterface, CancellablePromiseInt
     public function otherwise(callable $onRejected)
     {
         if (!_checkTypehint($onRejected, $this->reason)) {
-            return new RejectedPromise($this->reason);
+            return $this;
         }
 
         return $this->then(null, $onRejected);
@@ -65,7 +65,7 @@ class RejectedPromise implements ExtendedPromiseInterface, CancellablePromiseInt
 
     public function progress(callable $onProgress)
     {
-        return new RejectedPromise($this->reason);
+        return $this;
     }
 
     public function cancel()
